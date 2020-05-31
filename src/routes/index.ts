aa499f3dbe483102d7e8913a5006e64f95d1af6f
Gustavo Morini <gustavommh@gmail.com>
2020-05-31 09:33:43 -0300

    rota users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import transactionRouter from './transaction.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/transactions', transactionRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
