95c7b0dc40a9ff72a586346da82ecd7b5fbdacac
Gustavo Morini <gustavommh@gmail.com>
2020-05-31 13:43:51 -0300

    token jwt

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,6 @@
 import { getRepository } from 'typeorm';
-
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -11,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 export default class AuthenticateUserService {
@@ -29,6 +30,11 @@ export default class AuthenticateUserService {
       throw new Error('Incorrect e-mail/password combination.');
     }
 
-    return { user };
+    const token = sign({}, '4daaed10d00f88929b0516a1028b8cb3', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
